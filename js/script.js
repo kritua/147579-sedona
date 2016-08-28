0be443f8b8612b5fbea656d1abd038e8a33553bd
Kri Tua <kritua2@gmail.com>
2016-08-29 00:04:38 +0300

    Changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-var buttonChoose = document.querySelector('#choose');
-var elem = document.querySelector('.hotel-choose');
+var buttonChoose = document.querySelector("#choose");
+var elem = document.querySelector(".hotel-choose");
 
 buttonChoose.onclick = function() {
-	if (elem.style.display == 'block') {
-		elem.style.display = 'none';
+	buttonChoose.removeAttribute("href");
+	if (elem.style.display == "block") {
+		elem.style.display = "none";
 	} else {
-		elem.style.display = 'block';
+		elem.style.display = "block";
 	}
 };
 
